a04cf6025fb2097aacf52cfc7728bb4c9ccce29c
天琢 任 <770325525@qq.com>
2018-10-22 18:17:00 -0500

    Finishing up BioPage

--- AboutMeApp/AboutMeApp/MainPage.xaml.cs
+++ AboutMeApp/AboutMeApp/MainPage.xaml.cs
@@ -18,5 +18,20 @@ namespace AboutMeApp
         {
             Navigation.PushAsync(new ABoutMEPage() );
         }
+
+        private void NavigationButton2_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Skill ());
+        }
+
+        private void NavigationButton3_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Contact ());
+        }
+
+        private void NavigationButton4_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new MyBio());
+        }
     }
 }
